de1488b6e3cdeca8b50ef23077f4a65feb5df0ff
MonIQdev <MonIQdev1@gmail.com>
2026-08-21 17:07:54 -0400

    Create label.tsx

---
+++ lib/components/ui/label.tsx
@@ -0,0 +1,3 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+export const Label = ({className,...props}: any) => <label className={cn("text-sm font-medium", className)} {...props} />
