0836c7cc284994d684277dbbf6feb742165c5fef
MonIQdev <MonIQdev1@gmail.com>
2026-08-21 18:22:24 -0400

    Update route.ts

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -2,7 +2,7 @@ import { headers } from 'next/headers';
 import { prisma } from '@/lib/prisma';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2023-10-16' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-04-10' });
 
 export async function POST(req: Request) {
   const body = await req.text();
